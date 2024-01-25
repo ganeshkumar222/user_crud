df1e3da481aec2cef60f156efe058bdff9805d66
ganeshkumar222 <ganesh.gmv004@gmail.com>
2024-01-25 12:48:44 +0530

    expresss_crud_db

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import AppRoutes from './src/routes/index.js'
 let app = express()
-let port = 8000
+let PORT = process.env.PORT || 8000
 
 
 app.use(express.json())
 app.use("/",AppRoutes)
 
-app.listen(port,()=>console.log(`app is listening to port ${port}`))
+app.listen(PORT,()=>console.log(`app is listening to port ${PORT}`))
